519256c15f4b77ff3360fac92db75c0bf1771ce2
MaximeJdDS <48825681+MaximeJdDS@users.noreply.github.com>
2023-02-18 12:06:59 +0100

    Nombre de Fermat

--- nombredeFermat/src/CorrFermat.py
+++ nombredeFermat/src/CorrFermat.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import math
 
+def estpremier(n): 
+        for i in range(2,int(math.sqrt(n))+1):
+            if (n%i) == 0:
+                return False
+        return True
 
 
 def F(n):
-    Fermat = 2**(2*n) + 1
+    Fermat = 2**(2**n) + 1
     return Fermat
     
     
 def contreExemple():
-	i = 1
-	while(est_premier(F(n))):
-		i = i+1
-	return F(i)
+    return 4294967297
--- nombredeFermat/src/TestFermat.py
+++ nombredeFermat/src/TestFermat.py
@@ -9,15 +9,23 @@ import math
 import CorrFermat as corr
 import Fermat
 
+def estpremier(n): 
+    for i in range(2,int(math.sqrt(n))+1):
+        if (n%i) == 0: return False
+        return True
 
-#code found at https://geekflare.com/prime-number-in-python/
-def est_premier(n): 
-	for i in range(2,int(math.sqrt(n))+1):
-		if (n%i) == 0:
-			return False
-	return True
+def isFermat(n):
+    i    = 1
+    temp = corr.F(i)
+    while temp < n:
+        i = i+1
+        temp = corr.F(i)
+    if temp == n : return True
+    else : return False
+
 
 class TestFermat(unittest.TestCase):
+    
     
     def test_Fermat_exists(self):
         self.assertTrue(hasattr(Fermat, 'F'), _("Tu n\'as pas defini la bonne fonction. La fonction \'F(n)\' est introuvable."))
@@ -26,7 +34,7 @@ class TestFermat(unittest.TestCase):
         self.assertTrue(hasattr(Fermat, 'contreExemple'), _("Tu n\'as pas defini la bonne fonction. La fonction \'contreExemple()\' est introuvable."))
 
     def test_Fermat(self):
-        a = [random.randint(1, 20) for _ in range(8)]
+        a = [random.randint(1, 3) for _ in range(2)]
         ansFermat   = _("Le {} ieme nombre de Fermat est {} et tu as renvoyé {}.")
         #ansUnpair = _("L entier {} n est pas pair.")
         for i in range(len(a)):
@@ -36,13 +44,14 @@ class TestFermat(unittest.TestCase):
             
 
     def test_Prime(self):
-        ansPrime   = _("Le premier nombre de Fermat qui n'est pas premier est {}, ta fonction a renvoyé {}.")
-        for i in range(len(a)):
-            stu_ans  = Fermat.contreExemple()
-            corr_ans = corr.contreExemple()
-            self.assertEqual(corr_ans, stu_ans, ansPrime.format(corr_ans,stu_ans))
-            
-
+        ansPrime   = _("Ta fonction a renvoyé {}. Ce nombre est premier. Pour rappel, on cherche le premier nombre de Fermat qui n'est pas premier. N'hesite pas à utiliser la fonction \'estpremier(n)\'.")
+        ansNotFermat = _("Ta fonction a renvoyé {}. Ce nombre n'est pas un nombre de Fermat. Pour rappel, on cherche le premier nombre de Fermat qui n'est pas premier. N'hesite pas a utiliser la fonction \'F(n)\' que tu as défini précédemment.")
+        stu_ans  = Fermat.contreExemple()
+        corr_ans = corr.contreExemple()
+        if not (isFermat(stu_ans)):
+            self.assertEqual(corr_ans, stu_ans, ansNotFermat.format(stu_ans))
+        if estpremier(stu_ans):
+            self.assertEqual(corr_ans, stu_ans, ansPrime.format(stu_ans))
 
 if __name__ == '__main__':
     unittest.main()
