cdb5945513424af6ea27131c420eaf37b42d03dc
MaximeJdDS <48825681+MaximeJdDS@users.noreply.github.com>
2023-11-06 15:59:55 +0100

    Minuscule Exercices + CI

---
+++ Ch6Act4exo1/src/Test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import unittest
+import random
+
+import Corr as corr
+import student
+
+
+class Test(unittest.TestCase):
+    def test_exists(self):
+        self.assertTrue(hasattr(student, 'minuscule'), _("Tu n\'as pas defini la bonne fonction. La fonction 'minuscule' est introuvable."))
+
+    def test_CasDeBase(self):
+        a = [random.randint(65, 90) for _ in range(15)]
+        ansresult   = _("Pour l\'élément {} la réponse attendu est {} et ta fonction a renvoyé {}.")
+        
+        for i in range(len(a)):
+            stu_ans  = student.fonction(chr(a[i]))
+            corr_ans = corr.fonction(chr(a[i]))
+            self.assertEqual(corr_ans, stu_ans, ansresult.format(chr(a[i]),corr_ans,stu_ans))
+            
+            
+    def test_CasSpecial(self):
+        a = [random.randint(33, 127) for _ in range(35)]
+        ansresult   = _("Pour l\'élément {} la réponse attendu est {} et ta fonction a renvoyé {}.\n Qu'est-ce qu'on fait quand il y a des cas oú l'on voudrait faire quelque chose mais pour un autre cas, autre chose ?")
+        
+        for i in range(len(a)):
+            stu_ans  = student.fonction(chr(a[i]))
+            corr_ans = corr.fonction(chr(a[i]))
+            self.assertEqual(corr_ans, stu_ans, ansresult.format(chr(a[i]),corr_ans,stu_ans))
+
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
