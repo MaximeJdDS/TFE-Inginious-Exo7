80749780d90060b9875ccadff565d2cf7600fbce
MaximeJdDS <48825681+MaximeJdDS@users.noreply.github.com>
2024-03-26 14:56:30 +0100

    CI test for AssignCompares

--- $common/test_functions.py
+++ $common/test_functions.py
@@ -3,3 +3,30 @@ import pytest
 
 def test_1():
     assert 1==1
+
+def test_assignCompares():
+    # Exemples d'utilisation
+    code_exemple1 = """
+    if x = 4:
+        pass
+    """
+
+    code_exemple2 = """
+    if (x = 4):
+        pass
+    """
+
+    code_exemple3 = """
+    if (y == 5):
+        pass
+    """
+
+    code_exemple4 = """
+    if x == 4:
+        pass
+    """
+
+    assert assignCompares(code_exemple1) == True
+    assert assignCompares(code_exemple2) == True
+    assert assignCompares(code_exemple3) == False
+    assert assignCompares(code_exemple4) == False
